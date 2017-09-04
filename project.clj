7039e983dd2e39961209574ab4b0382e0415d28f
chendan <dan.chen@mtime.com>
2017-09-04 15:36:39 +0800

    add version1

--- project.clj
+++ project.clj
@@ -1,9 +1,22 @@
 (defproject clojure-ring-app "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot clojure-ring-app.core
+  :url "http://github.com/carolove/clojure-ring-app"
+  :license {:name "Apache License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/java.jdbc "0.7.1"]
+                 [mysql/mysql-connector-java "5.1.35"]
+                 [korma "0.4.3"]
+                 [selmer "1.11.0"]
+                 [ring "1.6.2"]
+                 [ring/ring-json "0.4.0"]
+                 [compojure "1.6.0"]]
+  :main  clojure-ring-app.web
+  :aot [clojure-ring-app.web]
+  :plugins [[lein-ring "0.12.1"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :ring {:handler clojure-ring-app.web/app
+         :auto-reload? true
+         :auto-refresh? true})
